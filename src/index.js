82965a0a4676ffe0ed441e1d713aeb250e683fe8
harshvardhan058 <harshvardhanmakwana786@gmail.com>
2023-11-07 12:30:09 +0530

    Database Connection Module Changed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
-// require("dotenv").config({path:'./.env'})
 import dotenv from "dotenv"
 dotenv.config({
     path:"./.env"
 })
-import mongoose from "mongoose";
 import express from "express";
-import { DB_NAME } from "./constants.js";
-
+import connectDB from "./db/index.js";
 const app = express()
 
+connectDB().then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listing on port ${process.env.PORT}`)
+        console.log(`App is runnign on process is ${process.pid}`)
+    })
+})
 
 
-;(async ()=>{
-    try{
-        console.log(process.env.MONGODB_URI)
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",(error)=>{
-            console.log("Error",error);
-            throw error
-        })
-        app.listen(process.env.PORT,()=>{
-            console.log(`App is listing on port ${process.env.PORT}`)
-        })
-    }
-    catch(error){
-        console.error("Error",error)
-        throw error
-    }
-})()
+
+
+
+
+
+// ;(async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         .then(()=>{
+//             console.log("Database Connected")
+//             console.log(`process id of the currently executing program is ${process.pid}`)
+//         }).catch((error)=>{
+//             console.log("Error while connecting the database",error)
+//         })
+//         app.on("error",(error)=>{
+//             console.log("Error",error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listing on port ${process.env.PORT}`)
+//         })
+//     }
+//     catch(error){
+//         console.error("Error",error)
+//         throw error
+//     }
+// })()
